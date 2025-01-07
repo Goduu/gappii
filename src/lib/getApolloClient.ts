b44cd178f78197e6761befd7dce704bb833fc22e
Goduu <idcangussu@hotmail.com>
2025-01-07 11:25:11 +0100

    fix graphql endpoint

--- src/lib/getApolloClient.ts
+++ src/lib/getApolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const getApolloClient = () =>
   new ApolloClient({
-    uri: process.env.GRAPHQL_ENDPOINT, // Your GraphQL endpoint
+    uri: `${process.env.NEXT_PUBLIC_BASE_URL}${process.env.GRAPHQL_ENDPOINT}`, // Your GraphQL endpoint
     cache: new InMemoryCache(),
     ssrMode: true, // Important for Server Components
   });
